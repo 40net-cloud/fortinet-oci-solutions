316af013c38b284725f334a60141aa9d64d9e172
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2023-12-08 01:07:15 +0300

    Add files via upload

---
+++ FortiGate/Standalone/New-VCN/terraform/block.tf
@@ -0,0 +1,12 @@
+resource "oci_core_volume" "vm_volume" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.compartment_ocid
+  display_name        = "vm_volume"
+  size_in_gbs         = var.volume_size
+}
+
+resource "oci_core_volume_attachment" "vm_volume_attach" {
+  attachment_type = "paravirtualized"
+  instance_id     = oci_core_instance.FortiGate.id
+  volume_id       = oci_core_volume.vm_volume.id
+}
