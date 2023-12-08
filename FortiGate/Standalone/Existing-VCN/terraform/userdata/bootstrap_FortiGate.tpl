dc0034190a3bcf226afc0f98fdb69952c6b83aa3
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2023-12-08 18:27:38 +0300

    Add files via upload

---
+++ FortiGate/Standalone/Existing-VCN/terraform/userdata/bootstrap_FortiGate.tpl
@@ -0,0 +1,62 @@
+Content-Type: multipart/mixed; boundary="==OCI=="
+MIME-Version: 1.0
+
+--==OCI==
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+
+config system global
+    set hostname "FortiGate"
+    set admintimeout 60
+end
+
+config system interface
+    edit port1
+        set ip ${untrust_ip} ${untrust_ip_mask}
+        set allowaccess ping https ssh http fgfm
+        set type physical
+        set alias mgmt
+        set description "untrust"
+        set mtu-override enable
+        set mtu 9000
+    next
+end
+config system interface
+    edit port2
+        set mode static        
+        set vdom "root"
+        set ip ${trust_ip} ${trust_ip_mask}
+        set type physical
+        set description "Trust"
+        set alias trust
+        set mtu-override enable
+        set mtu 9000
+    next
+end
+
+config system sdn-connector
+    edit "oci-sdn"
+        set type oci
+		set ha-status enable
+        set tenant-id ${tenancy_ocid}
+        set compartment-id ${compartment_ocid}
+    next
+end
+config router static
+    edit 1        
+        set gateway ${untrust_subnet_gw}
+        set device port1
+    next
+    edit 2
+        set dst ${vcn_cidr}
+        set gateway ${trust_subnet_gw}
+        set device port2
+    next
+end
+--==OCI==
+Content-Type: text/plain; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+
+--==OCI==--
+
---
+++ FortiGate/Standalone/Existing-VCN/terraform/variables.tf
@@ -0,0 +1,87 @@
+variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
+variable "user_ocid" {
+  default = ""
+}
+variable "private_key_path" {
+  default = ""
+}
+variable "fingerprint" {
+  default = ""
+}
+variable "region" {
+  description = "Oracle Cloud region"
+}
+
+##VCN and SUBNET ADDRESSESS
+variable "vcn_cidr" {
+  description = "Enter your VCN CIDR"
+}
+
+variable "vcn_ocid" {
+  description = "Enter your VCN OCID"
+}
+
+variable "igw_ocid" {
+  description = "Enter your Internet Gateway OCID"
+}
+
+variable "untrust_subnet_cidr" {
+  default = "10.1.1.0/24"
+}
+
+variable "untrust_subnet_gateway" {
+  default = "10.1.1.1"
+}
+
+variable "trust_subnet_cidr" {
+  default = "10.1.100.0/24"
+}
+
+variable "trust_subnet_gateway" {
+  default = "10.1.100.1"
+}
+
+#FIREWALL IPs
+
+variable "untrust_private_ip" {
+  default = "10.1.1.10"
+}
+
+variable "trust_private_ip" {
+  default = "10.1.100.10"
+}
+
+variable "vm_image_ocid" { 
+  default =  "ocid1.image.oc1..aaaaaaaa4d4x7cmgq4yimk6eg3kydwelpuctbxracvqcqktgza7zpmujan2a"
+}
+
+variable "mp_listing_id" {
+  default = "ocid1.appcataloglisting.oc1..aaaaaaaa6d5wbjlrlihw7l33nvdso74lv2s66snabevr33awotpgjownggiq" //byol
+}
+
+// Version
+variable "mp_listing_resource_version" {
+  default = "7.2.3_(_X64_)"
+}
+variable "instance_shape" {
+  default = "VM.Standard2.4"
+}
+
+# Choose an Availability Domain (1,2,3)
+variable "availability_domain" {
+  default = "1"
+}
+
+variable "volume_size" {
+  default = "50" //GB
+}
+
+variable "bootstrap_FortiGate" {
+ default = "./userdata/bootstrap_FortiGate.tpl"
+}
+
+variable "untrust_public_ip_lifetime" {
+  default = "RESERVED"
+  //or EPHEMERAL
+}
