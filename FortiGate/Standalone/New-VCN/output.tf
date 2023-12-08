5d6f66c62cb307d1d8092a5b97fbba3df50ebaba
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2023-12-08 18:13:06 +0300

    Delete FortiGate/Standalone/New-VCN/output.tf

--- FortiGate/Standalone/New-VCN/output.tf
+++
@@ -1,9 +0,0 @@
-# Output the private and public IPs of the instance
-
-output "Mgmt-FortiGate-PublicIP" {
-  value = [oci_core_instance.FortiGate.*.public_ip]
-}
-
-output "FortiGate-ID" {
-  value = [oci_core_instance.FortiGate.id]
-}
