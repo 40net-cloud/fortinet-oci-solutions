9d378efaf32eedc5e2a95e3f72d6324f58212a66
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2023-12-11 19:06:46 +0300

    Delete FortiManager/Existing-VCN/BYOL/output.tf

--- FortiManager/Existing-VCN/BYOL/output.tf
+++
@@ -1,9 +0,0 @@
-# Output the private and public IPs of the instance
-
-output "Mgmt-FortiManager-PublicIP" {
-  value = [oci_core_instance.FortiManager.*.public_ip]
-}
-
-output "FortiManager-ID" {
-  value = [oci_core_instance.FortiManager.id]
-}
