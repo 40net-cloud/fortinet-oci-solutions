613bacf29086ace30282b81aade3220a55fcee2b
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2025-10-10 13:48:21 +0300

    Add files via upload

---
+++ FortiGate/Active-Passive/terraform/compute.tf
@@ -0,0 +1,305 @@
+########################################
+###### Create FortiGate Primary VM #####
+########################################
+
+resource "oci_core_instance" "vm-a" {
+  count = local.matched_package != null ? 1 : 0
+  availability_domain = ( var.availability_domain_name_1 != "" ? var.availability_domain_name_1 : ( length(data.oci_identity_availability_domains.ads.availability_domains) == 1 ? data.oci_identity_availability_domains.ads.availability_domains[0].name : data.oci_identity_availability_domains.ads.availability_domains[count.index].name))
+  compartment_id      = var.compute_compartment_ocid
+  display_name        = "FortiGate-Primary-Firewall"
+  shape               = local.vm_compute_shape
+
+  dynamic "shape_config" {
+    for_each = local.vm_compute_shape != null && contains(["VM.Standard.A1.Flex", "VM.Standard.E4.Flex"], local.vm_compute_shape) ? [1] : []
+    content {
+      memory_in_gbs = var.memory_in_gbs
+      ocpus         = var.ocpu_count
+    }
+  }
+
+  create_vnic_details {
+    subnet_id        = local.use_existing_network ? var.management_subnet_id : oci_core_subnet.management_subnet[0].id
+    display_name     = "vm-a"
+    assign_public_ip = true
+    hostname_label   = "vma"
+    private_ip       = var.mgmt_private_ip_primary_a
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.license_type == "BYOL" ? local.matched_package.image_id : local.paygo_image_id
+  }
+  metadata = {
+    //ssh_authorized_keys = var.ssh_public_key
+    user_data           = base64encode(data.template_file.vm-a_userdata.rendered)
+  }
+
+  timeouts {
+    create = "60m"
+  }
+}
+
+##########################
+##### Untrust VNIC-A #####
+##########################
+resource "oci_core_vnic_attachment" "vnic_attach_untrust_a" {
+  count = 1
+  depends_on   = [oci_core_instance.vm-a]
+  instance_id  = oci_core_instance.vm-a[0].id
+  display_name = "vnic_untrust_a"
+
+  create_vnic_details {
+    subnet_id              = local.use_existing_network ? var.untrust_subnet_id : oci_core_subnet.untrust_subnet[0].id
+    display_name           = "vnic_untrust_a"
+    assign_public_ip       = true
+    skip_source_dest_check = false
+    private_ip             = var.untrust_private_ip_primary_a
+  }
+}
+
+resource "oci_core_private_ip" "untrust_private_ip" {
+  vnic_id      = data.oci_core_vnic_attachments.untrust_attachments.vnic_attachments.0.vnic_id
+  display_name   = "untrust_ip"
+  hostname_label = "untrust"
+  ip_address     = var.untrust_floating_private_ip
+}
+
+resource "oci_core_public_ip" "untrust_public_ip" {
+  compartment_id = var.compute_compartment_ocid
+  lifetime       = var.untrust_public_ip_lifetime
+  display_name   = "vm-untrust"
+  private_ip_id  = oci_core_private_ip.untrust_private_ip.id
+}
+
+########################
+##### Trust VNIC-A #####
+########################
+resource "oci_core_vnic_attachment" "vnic_attach_trust_a" {
+  depends_on   = [oci_core_vnic_attachment.vnic_attach_untrust_a]
+  count = 1
+  instance_id  = oci_core_instance.vm-a[count.index].id
+  display_name = "vnic_trust"
+
+  create_vnic_details {
+    subnet_id              = local.use_existing_network ? var.trust_subnet_id : oci_core_subnet.trust_subnet[0].id
+    display_name           = "vnic_trust_a"
+    assign_public_ip       = false
+    skip_source_dest_check = true
+    private_ip             = var.trust_private_ip_primary_a
+  }
+}
+
+resource "oci_core_private_ip" "trust_private_ip" {
+  vnic_id      = data.oci_core_vnic_attachments.trust_attachments.vnic_attachments.0.vnic_id
+  display_name   = "trust_ip"
+  hostname_label = "trust"
+  ip_address     = var.trust_floating_private_ip
+}
+
+##########################
+##### HA/Sync VNIC-A #####
+##########################
+resource "oci_core_vnic_attachment" "vnic_attach_hb_a" {
+  depends_on   = [oci_core_vnic_attachment.vnic_attach_trust_a]
+  count = 1
+  instance_id  = oci_core_instance.vm-a[count.index].id
+  display_name = "vnic_hb_a"
+
+  create_vnic_details {
+    subnet_id              = local.use_existing_network ? var.ha_subnet_id : oci_core_subnet.ha_subnet[0].id
+    display_name           = "vnic_hb_a"
+    assign_public_ip       = false
+    skip_source_dest_check = true
+    private_ip             = var.hb_private_ip_primary_a
+  }
+}
+
+#######################################
+##### Primary FortiGate Bootstrap #####
+#######################################
+data "template_file" "vm-a_userdata" {
+  template = file(var.bootstrap_vm-a)
+  vars = {
+    mgmt_ip = var.mgmt_private_ip_primary_a
+    mgmt_ip_mask = "255.255.255.0"
+    untrust_ip_a = var.untrust_private_ip_primary_a
+    untrust_ip_a_mask = "255.255.255.0"
+    trust_ip_a = var.trust_private_ip_primary_a
+    trust_ip_a_mask = "255.255.255.0"
+    hb_ip = var.hb_private_ip_primary_a
+    hb_ip_mask = "255.255.255.0"
+    hb_peer_ip = var.hb_private_ip_primary_b
+    untrust_floating_private_ip = var.untrust_floating_private_ip
+    untrust_floating_private_ip_mask = "255.255.255.0"
+    trust_floating_private_ip = var.trust_floating_private_ip
+    trust_floating_private_ip_mask = "255.255.255.0"
+    untrust_subnet_gw = var.untrust_subnet_gateway
+    vcn_cidr = var.vcn_cidr_block
+    trust_subnet_gw = var.trust_subnet_gateway
+    mgmt_subnet_gw = var.mgmt_subnet_gateway
+    tenancy_ocid = var.tenancy_ocid
+    compartment_ocid = var.compute_compartment_ocid
+  }
+}
+
+#######################################
+##### Primary FortiGate Volumes #####
+#######################################
+
+resource "oci_core_volume" "vm_volume-a" {
+  count = 1
+  availability_domain = ( var.availability_domain_name_1 != "" ? var.availability_domain_name_1 : ( length(data.oci_identity_availability_domains.ads.availability_domains) == 1 ? data.oci_identity_availability_domains.ads.availability_domains[0].name : data.oci_identity_availability_domains.ads.availability_domains[count.index].name))
+  compartment_id      = var.compute_compartment_ocid
+  display_name        = "vm_volume-a"
+  size_in_gbs         = var.volume_size
+}
+
+resource "oci_core_volume_attachment" "vm_volume_attach-a" {
+  count = length(oci_core_instance.vm-a) > 0 ? 1 : 0
+  attachment_type = "paravirtualized"
+  instance_id = oci_core_instance.vm-a[0].id
+  volume_id       = oci_core_volume.vm_volume-a[count.index].id
+}
+
+##########################################
+###### Create FortiGate Secondary VM #####
+##########################################
+resource "oci_core_instance" "vm-b" {
+  depends_on          = [oci_core_subnet.ha_subnet]
+  count = local.matched_package != null ? 1 : 0
+  availability_domain = ( var.availability_domain_name_2 != "" ? var.availability_domain_name_2 : ( length(data.oci_identity_availability_domains.ads.availability_domains) == 1 ? data.oci_identity_availability_domains.ads.availability_domains[0].name : data.oci_identity_availability_domains.ads.availability_domains[count.index].name))
+  compartment_id      = var.compute_compartment_ocid
+  display_name        = "FortiGate-Secondary-Firewall"
+  shape               = local.vm_compute_shape
+
+  dynamic "shape_config" {
+    for_each = local.vm_compute_shape != null && contains(["VM.Standard.A1.Flex", "VM.Standard.E4.Flex"], local.vm_compute_shape) ? [1] : []
+    content {
+      memory_in_gbs = var.memory_in_gbs
+      ocpus         = var.ocpu_count
+    }
+  }
+  
+  create_vnic_details {
+    subnet_id        = local.use_existing_network ? var.management_subnet_id : oci_core_subnet.management_subnet[0].id
+    display_name     = "vm-b"
+    assign_public_ip = true
+    hostname_label   = "vmb"
+    private_ip       = var.mgmt_private_ip_primary_b
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.license_type == "BYOL" ? local.matched_package.image_id : local.paygo_image_id
+  }
+  metadata = {
+    //ssh_authorized_keys = var.ssh_public_key
+    user_data           = base64encode(data.template_file.vm-b_userdata.rendered)
+  }
+
+  timeouts {
+    create = "60m"
+  }
+}
+
+##########################
+##### Untrust VNIC-B #####
+##########################
+resource "oci_core_vnic_attachment" "vnic_attach_untrust_b" {
+  depends_on   = [oci_core_instance.vm-b]
+  count = 1
+  instance_id  = oci_core_instance.vm-b[0].id
+  display_name = "vnic_untrust_b"
+
+  create_vnic_details {
+    subnet_id              = local.use_existing_network ? var.untrust_subnet_id : oci_core_subnet.untrust_subnet[0].id
+    display_name           = "vnic_untrust_b"
+    assign_public_ip       = true
+    skip_source_dest_check = false
+    private_ip             = var.untrust_private_ip_primary_b
+  }
+}
+
+########################
+##### Trust VNIC-B #####
+########################
+resource "oci_core_vnic_attachment" "vnic_attach_trust_b" {
+  depends_on   = [oci_core_vnic_attachment.vnic_attach_untrust_b]
+  count = 1
+  instance_id  = oci_core_instance.vm-b[count.index].id
+  display_name = "vnic_trust"
+
+  create_vnic_details {
+    subnet_id              = local.use_existing_network ? var.trust_subnet_id : oci_core_subnet.trust_subnet[0].id
+    display_name           = "vnic_trust_b"
+    assign_public_ip       = false
+    skip_source_dest_check = true
+    private_ip             = var.trust_private_ip_primary_b
+  }
+}
+
+##########################
+##### HA/Sync VNIC-B #####
+##########################
+resource "oci_core_vnic_attachment" "vnic_attach_hb_b" {
+  depends_on   = [oci_core_vnic_attachment.vnic_attach_trust_b]
+  count = 1
+  instance_id  = oci_core_instance.vm-b[count.index].id
+  display_name = "vnic_hb_b"
+
+  create_vnic_details {
+    subnet_id              = local.use_existing_network ? var.ha_subnet_id : oci_core_subnet.ha_subnet[0].id
+    display_name           = "vnic_hb_b"
+    assign_public_ip       = false
+    skip_source_dest_check = true
+    private_ip             = var.hb_private_ip_primary_b
+  }
+}
+
+#########################################
+##### Secondary FortiGate Bootstrap #####
+#########################################
+data "template_file" "vm-b_userdata" {
+  template = file(var.bootstrap_vm-b)
+
+  vars = {
+    mgmt_ip = var.mgmt_private_ip_primary_b
+    mgmt_ip_mask = "255.255.255.0"
+    untrust_ip_b = var.untrust_private_ip_primary_b
+    untrust_ip_b_mask = "255.255.255.0"
+    trust_ip_b = var.trust_private_ip_primary_b
+    trust_ip_b_mask = "255.255.255.0"
+    hb_ip = var.hb_private_ip_primary_b
+    hb_ip_mask = "255.255.255.0"
+    hb_peer_ip = var.hb_private_ip_primary_a
+    untrust_floating_private_ip = var.untrust_floating_private_ip
+    untrust_floating_private_ip_mask = "255.255.255.0"
+    trust_floating_private_ip = var.trust_floating_private_ip
+    trust_floating_private_ip_mask = "255.255.255.0"
+    untrust_subnet_gw = var.untrust_subnet_gateway
+    vcn_cidr = var.vcn_cidr_block
+    trust_subnet_gw = var.trust_subnet_gateway
+    mgmt_subnet_gw = var.mgmt_subnet_gateway
+    tenancy_ocid     = var.tenancy_ocid
+    compartment_ocid = var.compute_compartment_ocid
+  }
+}
+
+#######################################
+##### Secondary FortiGate Volumes #####
+#######################################
+
+resource "oci_core_volume" "vm_volume-b" {
+  count = 1
+  availability_domain = ( var.availability_domain_name_2 != "" ? var.availability_domain_name_2 : ( length(data.oci_identity_availability_domains.ads.availability_domains) == 1 ? data.oci_identity_availability_domains.ads.availability_domains[0].name : data.oci_identity_availability_domains.ads.availability_domains[count.index].name))
+  compartment_id      = var.compute_compartment_ocid
+  display_name        = "vm_volume-b"
+  size_in_gbs         = var.volume_size
+}
+
+resource "oci_core_volume_attachment" "vm_volume_attach-b" {
+  count = length(oci_core_instance.vm-b) > 0 ? 1 : 0
+  attachment_type = "paravirtualized"
+  instance_id = oci_core_instance.vm-b[0].id
+  volume_id       = oci_core_volume.vm_volume-b[count.index].id
+}
---
+++ FortiGate/Active-Passive/terraform/variables.tf
@@ -0,0 +1,432 @@
+#Variables declared in this file must be declared in the marketplace.yaml
+
+############################
+#  Hidden Variable Group   #
+############################
+
+variable "tenancy_ocid" {
+  description = "OCI Tenancy OCID"
+}
+
+variable "compute_compartment_ocid" {
+  description = "Compartment where Compute and Marketplace subscription resources will be created"
+}
+
+variable "user_ocid" {
+  description = "OCI User OCID"
+  default = ""
+}
+
+variable "private_key_path" {
+  description = "Path to the private key for OCI API authentication"
+  default = ""
+}
+
+variable "fingerprint" {
+  description = "Fingerprint of the public key for OCI API authentication"
+  default = ""
+}
+
+variable "region" {
+  description = "Oracle Cloud region (e.g., us-ashburn-1)"
+}
+
+#######################
+#  Marketplace Image  #
+#######################
+
+variable "mp_subscription_enabled" {
+  description = "Subscribe to Marketplace listing?"
+  type        = bool
+  default     = true
+}
+
+############################
+#  Compute Configuration   #
+############################
+
+variable "vm_display_name" {
+  description = "Instance Name"
+  default     = "FortiGate"
+}
+
+variable "vm_flex_shape_ocpus" {
+  description = "Flex Shape OCPUs"
+  default     = 4
+}
+
+variable "vm_compute_shape_arm" {
+  description = "Compute shape to use when CPU type is ARM64"
+  type        = string
+  default     = ""
+}
+
+variable "vm_compute_shape_x64" {
+  description = "Compute shape to use when CPU type is X64"
+  type        = string
+  default     = ""
+}
+
+variable "availability_domain_number" {
+  type        = number
+  description = "Optional AD number input"
+  default     = 1
+}
+
+variable "availability_domain_name_1" {
+  description = "Availability Domain for Member-A"
+}
+
+variable "availability_domain_name_2" {
+  description = "Availability Domain for Member-B"
+}
+
+variable "license_type" {
+  description = "License type, e.g. BYOL or PAYGO (like 'PAYGO 4 OCPUs')"
+  type        = string
+}
+
+variable "ocpu_count" {
+  description = "Number of OCPUs for Flex shapes"
+  type        = number
+  nullable    = true
+  default     = null
+}
+
+variable "memory_in_gbs" {
+  description = "Amount of memory (GB) for Flex shapes"
+  type        = number
+  nullable    = true
+  default     = null
+}
+
+variable "cpu_type" {
+  type    = string
+  validation {
+    condition     = contains(["X64", "ARM64"], var.cpu_type)
+    error_message = "cpu_type must be X64 or ARM64"
+  }
+}
+
+variable "fortios_version" {
+  type    = string
+  validation {
+    condition     = contains(["6.4.13", "7.0.17", "7.2.11", "7.4.8", "7.6.3"], var.fortios_version)
+    error_message = "Only supported FortiOS versions are allowed"
+  }
+}
+
+variable "instance_launch_options_network_type" {
+  description = "NIC Attachment Type"
+  default     = "PARAVIRTUALIZED"
+}
+
+############################
+#  Network Configuration   #
+############################
+
+variable "network_strategy" {
+  default = "Create New VCN and Subnet"
+}
+
+variable "vcn_id" {
+  default = ""
+}
+
+variable "vcn_display_name" {
+  description = "VCN Name"
+  default     = "FortiGate-Hub-VCN"
+}
+
+variable "vcn_cidr_block" {
+  description = "VCN CIDR"
+  default     = "192.168.0.0/16"
+}
+
+variable "vcn_dns_label" {
+  description = "VCN DNS Label"
+  default     = "fortigateha"
+}
+
+variable "subnet_span" {
+  description = "Choose between regional and AD specific subnets"
+  default     = "Regional Subnet"
+}
+
+variable "management_subnet_id" {
+  default = ""
+}
+
+variable "management_subnet_display_name" {
+  description = "Management Subnet Name"
+  default     = "mgmt-subnet"
+}
+
+variable "management_subnet_cidr_block" {
+  description = "Management Subnet CIDR"
+  default     = "192.168.1.0/24"
+}
+
+variable "management_subnet_dns_label" {
+  description = "Management Subnet DNS Label"
+  default     = "management"
+}
+
+variable "trust_subnet_id" {
+  default = ""
+}
+
+variable "trust_subnet_display_name" {
+  description = "Trust Subnet Name"
+  default     = "trust-subnet"
+}
+
+variable "trust_subnet_cidr_block" {
+  description = "Trust Subnet CIDR"
+  default     = "192.168.2.0/24"
+}
+
+variable "trust_subnet_dns_label" {
+  description = "Trust Subnet DNS Label"
+  default     = "trust"
+}
+
+variable "untrust_subnet_id" {
+  default = ""
+}
+
+variable "untrust_subnet_display_name" {
+  description = "Firewall Untrust Subnet Name"
+  default     = "untrust-subnet"
+}
+
+variable "untrust_subnet_cidr_block" {
+  description = "Firewall Untrust Subnet CIDR"
+  default     = "192.168.3.0/24"
+}
+
+variable "untrust_subnet_dns_label" {
+  description = "Untrust Subnet DNS Label"
+  default     = "untrust"
+}
+
+variable "ha_subnet_id" {
+  default = ""
+}
+
+variable "ha_subnet_display_name" {
+  description = "HA Subnet Name"
+  default     = "ha-subnet"
+}
+
+variable "ha_subnet_cidr_block" {
+  description = "HA Subnet CIDR"
+  default     = "192.168.4.0/24"
+}
+
+variable "ha_subnet_dns_label" {
+  description = "HA Subnet DNS Label"
+  default     = "ha"
+}
+
+variable "use_existing_ip" {
+  description = "Use an existing permanent public ip"
+  default     = "Create new IP"
+}
+
+variable "template_name" {
+  description = "Template name. Should be defined according to deployment type"
+  default     = "fortigate-drg-ha"
+}
+
+variable "template_version" {
+  description = "Template version"
+  default     = "20210701"
+}
+
+variable "igw_ocid" {
+  description = "Internet Gateway OCID"
+  default     = ""
+}
+
+############################
+# Security Configuration   #
+############################
+variable "nsg_whitelist_ip" {
+  description = "Network Security Groups - Whitelisted CIDR block for ingress communication: Enter 0.0.0.0/0 or <your IP>/32"
+  default     = "0.0.0.0/0"
+}
+
+variable "nsg_display_name" {
+  description = "Network Security Groups - Name"
+  default     = "cluster-security-group"
+}
+
+############################
+# Additional Configuration #
+############################
+
+#############################
+# Route Table Configuration #
+#############################
+
+variable "public_routetable_display_name" {
+  description = "Public route table Name"
+  default     = "Untrust-Route-Table"
+}
+
+variable "ha_routetable_display_name" {
+  description = "HA route table Name"
+  default     = "HA-Route-Table"
+}
+
+variable "management_routetable_display_name" {
+  description = "Management route table Name"
+  default     = "Management-Route-Table"
+}
+
+variable "management_routetable_display_name_existing" {
+  description = "Management route table Name"
+  default     = "Management-Route-Table"
+}
+
+variable "untrust_routetable_display_name_existing" {
+  description = "Untrust route table Name"
+  default     = "Untrust-Route-Table-Existing"
+}
+
+variable "trust_routetable_display_name_existing" {
+  description = "Untrust route table Name"
+  default     = "Untrust-Route-Table-Existing"
+}
+
+variable "ha_routetable_display_name_existing" {
+  description = "Untrust route table Name"
+  default     = "Untrust-Route-Table-Existing"
+}
+
+variable "management_route_table_existing_attachment" {
+  description = "Management route table existing attachment Name"
+  default     = "Management-Route-Table-Existing-Attachment"
+}
+
+variable "private_routetable_display_name" {
+  description = "Private route table Name"
+  default     = "Trust-Route-Table"
+}
+
+######################
+#    Enum Values     #   
+######################
+variable "network_strategy_enum" {
+  type = map
+  default = {
+    CREATE_NEW_VCN_SUBNET   = "Create New VCN and Subnet"
+    USE_EXISTING_VCN_SUBNET = "Use Existing VCN and Subnet"
+  }
+}
+
+variable "subnet_type_enum" {
+  type = map
+  default = {
+    transit_subnet    = "Private Subnet"
+    MANAGEMENT_SUBENT = "Public Subnet"
+  }
+}
+
+variable "nsg_config_enum" {
+  type = map
+  default = {
+    BLOCK_ALL_PORTS = "Block all ports"
+    OPEN_ALL_PORTS  = "Open all ports"
+    CUSTOMIZE       = "Customize ports - Post deployment"
+  }
+}
+
+variable "bootstrap_vm-a" {
+  default = "./cloudinit/bootstrap_vm-a.tpl"
+}
+
+variable "bootstrap_vm-b" {
+ default = "./cloudinit/bootstrap_vm-b.tpl"
+}
+
+######################
+#    Static Values     #   
+######################
+#ACTIVE NODE
+variable "mgmt_private_ip_primary_a" {
+  description = "Primary Firewall Mgmt Interface Private IP"
+  default = "192.168.1.10"
+}
+
+variable "untrust_private_ip_primary_a" {
+  description = "Primary Firewall Untrust Interface Private IP"
+  default = "192.168.3.10"
+}
+
+variable "trust_private_ip_primary_a" {
+  description = "Primary Firewall Trust Interface Private IP"
+  default = "192.168.2.10"
+}
+
+variable "hb_private_ip_primary_a" {
+  description = "Primary Firewall HA Interface Private IP"
+  default = "192.168.4.10"
+}
+
+#PASSIVE NODE
+variable "mgmt_private_ip_primary_b" {
+  description = "Secondary Firewall Mgmt Interface Private IP"
+  default = "192.168.1.20"
+}
+
+variable "untrust_private_ip_primary_b" {
+  description = "Secondary Firewall Untrust Interface Private IP"
+  default = "192.168.3.20"
+}
+
+variable "trust_private_ip_primary_b" {
+  description = "Secondary Firewall Trust Interface Private IP"
+  default = "192.168.2.20"
+}
+
+variable "hb_private_ip_primary_b" {
+  description = "Secondary Firewall HA Interface Private IP"
+  default = "192.168.4.20"
+}
+
+variable "untrust_floating_private_ip" {
+  description = "Firewall Untrust Interface Floating Private IP"
+  default = "192.168.3.30"
+}
+
+variable "trust_floating_private_ip" {
+  description = "Firewall Trust Interface Floating Private IP"
+  default = "192.168.2.30"
+}
+
+variable "mgmt_subnet_gateway" {
+  description = "Mgmt Subnet Default Gateway IP"
+  default = "192.168.1.1"
+}
+
+variable "trust_subnet_gateway" {
+  description = "Trust Subnet Default Gateway IP"
+  default = "192.168.2.1"
+}
+
+variable "untrust_subnet_gateway" {
+  description = "Untrust Subnet Default Gateway IP"
+  default = "192.168.3.1"
+}
+
+variable "untrust_public_ip_lifetime" {
+  description = "Public IP Address Reservation Type"
+  default = "RESERVED"
+}
+
+variable "volume_size" {
+  description = "Firewall VM Block Volume Attachment Size in GB"
+  default = "50" //GB
+}
