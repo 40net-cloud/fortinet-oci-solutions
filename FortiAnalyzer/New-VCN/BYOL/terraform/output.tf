c79cfe256eabdc37e567bee035776149de2cb438
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2025-10-10 13:24:30 +0300

    Delete FortiAnalyzer/New-VCN/BYOL directory

--- FortiAnalyzer/New-VCN/BYOL/terraform/output.tf
+++
@@ -1,9 +0,0 @@
-# Output the private and public IPs of the instance
-
-output "Mgmt-FortiAnalyzer-PublicIP" {
-  value = [oci_core_instance.FortiAnalyzer.*.public_ip]
-}
-
-output "FortiAnalyzer-ID" {
-  value = [oci_core_instance.FortiAnalyzer.id]
-}
